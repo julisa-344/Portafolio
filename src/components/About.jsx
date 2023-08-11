5b1e262dc5c7af207f7a46b79b09e25c15c5b1d8
Julisa Leon <julisa.leon344@gmail.com>
2023-08-11 15:25:21 -0500

    first commit

---
+++ src/components/About.jsx
@@ -0,0 +1,63 @@
+import React from "react";
+import Tilt from "react-tilt";
+import { motion } from "framer-motion";
+
+import { styles } from "../styles";
+import { services } from "../constants";
+import { SectionWrapper } from "../hoc";
+import { fadeIn, textVariant } from "../utils/motion";
+
+const ServiceCard = ({ index, title, icon }) => (
+  <Tilt className='xs:w-[250px] w-full'>
+    <motion.div
+      variants={fadeIn("right", "spring", index * 0.5, 0.75)}
+      className='w-full green-pink-gradient p-[1px] rounded-[20px] shadow-card'
+    >
+      <div
+        options={{
+          max: 45,
+          scale: 1,
+          speed: 450,
+        }}
+        className='bg-tertiary rounded-[20px] py-5 px-12 min-h-[280px] flex justify-evenly items-center flex-col'
+      >
+        <img
+          src={icon}
+          alt='web-development'
+          className='w-16 h-16 object-contain'
+        />
+
+        <h3 className='text-white text-[20px] font-bold text-center'>
+          {title}
+        </h3>
+      </div>
+    </motion.div>
+  </Tilt>
+);
+
+const About = () => {
+  return (
+    <>
+      <motion.div variants={textVariant()}>
+        <p className={styles.sectionSubText}>Introducciòn</p>
+        <h2 className={styles.sectionHeadText}>Sobre mi</h2>
+      </motion.div>
+
+      <motion.p
+        variants={fadeIn("", "", 0.1, 1)}
+        className='mt-4 text-secondary text-[17px] max-w-3xl leading-[30px]'
+      >
+        Soy una entusiasta aprendiz del mundo del desarrollo web. Mi enfoque principal está en el diseño y desarrollo de páginas web, especialmente en el frontend, así como en la creación de experiencias de usuario atractivas (UX/UI) y el arte del diseño gráfico.
+        Tengo la capacidad de aprender rápidamente y colaborar estrechamente con los clientes para crear soluciones eficientes, escalables y amigables con el usuario que resuelvan problemas del mundo real.
+      </motion.p>
+
+      <div className='mt-20 flex flex-wrap gap-10'>
+        {services.map((service, index) => (
+          <ServiceCard key={service.title} index={index} {...service} />
+        ))}
+      </div>
+    </>
+  );
+};
+
+export default SectionWrapper(About, "about");
---
+++ src/constants/index.js
@@ -0,0 +1,208 @@
+import {
+  mobile,
+  backend,
+  creator,
+  web,
+  javascript,
+  typescript,
+  html,
+  css,
+  reactjs,
+  redux,
+  tailwind,
+  nodejs,
+  mongodb,
+  git,
+  figma,
+  docker,
+  meta,
+  starbucks,
+  tesla,
+  shopify,
+  carrent,
+  jobit,
+  tripguide,
+  threejs,
+  ux,
+  community,
+  tareas
+} from "../assets";
+
+export const navLinks = [
+  {
+    id: "about",
+    title: "Sobre mi",
+  },
+  {
+    id: "work",
+    title: "Trabajos",
+  },
+  {
+    id: "contact",
+    title: "Contacto",
+  },
+];
+
+const services = [
+  {
+    title: "Desarrollo Frontend",
+    icon: web,
+  },
+  {
+    title: "Diseño UX/UI",
+    icon: mobile,
+  },
+  {
+    title: "Diseño Gráfico",
+    icon: backend,
+  }
+];
+
+const technologies = [
+  {
+    name: "HTML 5",
+    icon: html,
+  },
+  {
+    name: "CSS 3",
+    icon: css,
+  },
+  {
+    name: "JavaScript",
+    icon: javascript,
+  },
+  {
+    name: "React JS",
+    icon: reactjs,
+  },
+  {
+    name: "Tailwind CSS",
+    icon: tailwind,
+  },
+  {
+    name: "Matiraliza",
+    icon: docker,
+  },
+  {
+    name: "Bootstrap",
+    icon: docker,
+  },
+  {
+    name: "figma",
+    icon: figma,
+  },
+  {
+    name: "Photoshop",
+    icon: docker,
+  },
+  {
+    name: "Illustrator",
+    icon: docker,
+  },
+  {
+    name: "Adobe xd",
+    icon: docker,
+  },
+];
+
+const experiences = [
+  {
+    title: "Estrategias Visuales en Redes Sociales",
+    company_name: "",
+    icon: community,
+    iconBg: "#383E56",
+    date: "2021",
+    points: [
+      "Desarrollé y gestioné publicaciones para Instagram en una empresa de tecnología, creando contenido visualmente atractivo y relevante que reflejaba la marca. ",
+    ],
+  },
+  {
+    title: "Desarrollo de Páginas Web en Entornos Educativos",
+    company_name: "",
+    icon: tareas,
+    iconBg: "#E6DEDD",
+    date: "2022",
+    points: [
+      "Llevé a cabo la creación de páginas web como parte de proyectos asignados durante mi proceso de formación en un bootcamp. Aplicando mis conocimientos en desarrollo frontend, diseñé y desarrollé sitios web interactivos que demostraron habilidades en la implementación de diseños, optimización de UX/UI y codificación funcional.",
+    ],
+  },
+  {
+    title: "Colaboración con Diseñadores de Interfaces:",
+    company_name: "",
+    icon: ux,
+    iconBg: "#383E56",
+    date: "2023",
+    points: [
+      "Trabajé en estrecha colaboración con diseñadores de interfaces en un estudio de diseño gráfico, contribuyendo a la implementación de diseños creativos y funcionales en diversas plataformas. ",
+    ],
+  }
+];
+
+
+const projects = [
+  {
+    name: "Car Rent",
+    description:
+      "Web-based platform that allows users to search, book, and manage car rentals from various providers, providing a convenient and efficient solution for transportation needs.",
+    tags: [
+      {
+        name: "react",
+        color: "blue-text-gradient",
+      },
+      {
+        name: "mongodb",
+        color: "green-text-gradient",
+      },
+      {
+        name: "tailwind",
+        color: "pink-text-gradient",
+      },
+    ],
+    image: carrent,
+    source_code_link: "https://github.com/",
+  },
+  {
+    name: "Job IT",
+    description:
+      "Web application that enables users to search for job openings, view estimated salary ranges for positions, and locate available jobs based on their current location.",
+    tags: [
+      {
+        name: "react",
+        color: "blue-text-gradient",
+      },
+      {
+        name: "restapi",
+        color: "green-text-gradient",
+      },
+      {
+        name: "scss",
+        color: "pink-text-gradient",
+      },
+    ],
+    image: jobit,
+    source_code_link: "https://github.com/",
+  },
+  {
+    name: "Trip Guide",
+    description:
+      "A comprehensive travel booking platform that allows users to book flights, hotels, and rental cars, and offers curated recommendations for popular destinations.",
+    tags: [
+      {
+        name: "nextjs",
+        color: "blue-text-gradient",
+      },
+      {
+        name: "supabase",
+        color: "green-text-gradient",
+      },
+      {
+        name: "css",
+        color: "pink-text-gradient",
+      },
+    ],
+    image: tripguide,
+    source_code_link: "https://github.com/",
+  },
+];
+
+export { services, technologies, experiences, projects };
