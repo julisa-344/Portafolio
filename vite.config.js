e647ac8fdcd96a8e9c5ae505822a3aba71fbaabf
Julisa Leon <julisa.leon344@gmail.com>
2023-08-16 17:41:25 -0500

    Creando carpeta dist

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: 'https://julisa-344.github.io/Portafolio'
+  base: '/Portafolio/',
 })
